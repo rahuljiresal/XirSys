bc876575e2346b3a77332e3bd1f16c564cd070aa
Sam Symons <sam@samsymons.com>
2014-07-28 09:43:40 -0700

    Update documentation on the client object.

--- Classes/XSClient.h
+++ Classes/XSClient.h
@@ -13,13 +13,23 @@ typedef void(^XSArrayCompletion)(NSArray *collection, NSError *error);
 
 @interface XSClient : NSObject
 
+/**
+ The username attached to your account. This will be used by each request made to the XirSys API.
+ */
 @property (nonatomic, copy) NSString *username;
+
+/**
+ The secret key attached to your account. This will be used by each request made to the XirSys API.
+ */
 @property (nonatomic, copy) NSString *secretKey;
 
 - (instancetype)initWithUsername:(NSString *)username secretKey:(NSString *)secretKey;
 
 - (NSURLSessionDataTask *)getTokenForDomain:(NSString *)domain application:(NSString *)application room:(NSString *)room secure:(BOOL)secure completion:(XSObjectCompletion)completion;
 
+/**
+ List all ICE servers for a given domain.
+ */
 - (NSURLSessionDataTask *)getIceServersForDomain:(NSString *)domain application:(NSString *)application room:(NSString *)room secure:(BOOL)secure completion:(XSObjectCompletion)completion;
 
 /**
